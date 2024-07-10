8c19572ae39c94d7ff6e7ccde6ce52f721ca4f0f
suvrat128 <92354425+suvrat128@users.noreply.github.com>
2024-07-10 23:27:38 +0530

    Update ques1.py

--- ques1.py
+++ ques1.py
@@ -15,3 +15,38 @@ print(c)
 4. adding 1 to c fro every element of CDT
 5. at last after complete iteration of loop printiong c value 
 '''
+
+#wpt print how many times given substring is present given string
+
+MS = input('enter the string:')
+SS = input('enter the sub string:')
+
+c = 0
+for el in MS:
+    if el == SS:
+        c+=1
+print(c)
+
+'''
+# process
+1. took main string as input
+2. took substring as input
+3. by assuming given substring is not present in mainstring c=0
+4. by using for loop
+    iteration1:
+        it will fetch 'h' so el= 'h'
+        check 'h' == 'o' false so still c=0
+    iteration2:
+        it will fetch 'e' so el= 'e'
+        check 'e' == 'o' false so still c=0
+    iteration3:
+        it will fetch 'l' so el= 'l'
+        check 'l' == 'o' false so still c=0
+    iteration4:
+        it will fetch 'l' so el= 'l'
+        check 'l' == 'o' false so still c=0
+    iteration1:
+        it will fetch 'o' so el= 'o'
+        check 'o' == 'o' true
+                    so incerment c from 0 to 1
+5.at last after complete iteration of loop printing c value'''
