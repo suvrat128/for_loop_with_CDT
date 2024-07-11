6abbe257f7f53272da612044cba010e40e16a0a4
suvrat128 <92354425+suvrat128@users.noreply.github.com>
2024-07-11 23:17:35 +0530

    Create 01_el_in_CDT.py

---
+++ 01_el_in_CDT.py
@@ -0,0 +1,20 @@
+# wpt check how many elements present in given CDT
+
+cdt = eval(input('enter a CDT:'))
+c=0
+for i in cdt:
+    c+=1
+print(c)
+
+
+'''
+# process
+1. took CDT as input
+2. By assuming given CDT is empty assigning c =0
+3. by using for loop fetching each and every element of CDT
+4. adding 1 to c fro every element of CDT
+5. at last after complete iteration of loop printiong c value 
+'''
+
+
+
